59c56864c947e91fc7159be5d964f4fd8000f738
Dio Rig <dio_rig@yahoo.com>
2019-02-06 08:07:03 +0200

    Created seed file for blogs posts portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+blogs = 10
+blogs.times do |n|
+	Blog.create!(
+		title: "My blog #{n}",
+		body: "#{n} steps exponentially lead to #{n * n}."
+		)
+end
+puts  "#{blogs} blogs created"
+
+skills = 5
+skills.times do |n|
+	Skill.create!(
+		title: "Ruby #{n}",
+		percent_spent: 20
+		)
+end
+puts  "#{skills} skills created"
+
+portfolios = 9
+portfolios.times do |n|
+	Portfolio.create!(
+		title: "Portfolio Item #{n}",
+		subtitle: "My fantastic service #{n}",
+        body: "#{n}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image: "https://via.placeholder.com/350x200?text=PHILOKRATOS #{n}",
+        thumb_image: "https://via.placeholder.com/600x400/0000FF/808080 ?Text=Digital.com"
+		)
+end
+puts  "#{portfolios} portfolio items created"
