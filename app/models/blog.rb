c75ad3d0518a207d7b74cdbf7ab54da8dc8597e1
Dio Rig <dio_rig@yahoo.com>
2019-02-14 20:19:26 +0200

    Added topics with relationship to blogs - each blog has a topic

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,9 @@ class Blog < ApplicationRecord
 	enum status: { draft: 0, published: 1 }
 	extend FriendlyId
   friendly_id :title, use: :slugged
+  attr_accessor :topic_id
 
   validates_presence_of :title, :body
+
+  belongs_to :topic
 end
